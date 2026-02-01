97888963892c674cefb78c672ff5d541b1507bd0
choneface <gavin.garcia0001@gmail.com>
2026-02-01 14:09:30 -0500

    switching to python because taylor said i am a try hard

--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/choneface/zxdf/cmd"
-)
-
-func main() {
-	cmd.Execute()
-}
---
+++ main.py
@@ -0,0 +1,28 @@
+from typing import Annotated
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def add(skill: str):
+    print(f"Adding {skill} to known skills")
+
+
+@app.command()
+def update(skill: Annotated[str, typer.Argument()] = ""):
+    if skill == "":
+        ans = input("Are you sure you want to update all skills? (Y/n): ")
+        if ans == "Y":
+            print("Updating all skills...") 
+            return
+        else:
+            print("No worries, exiting")
+            return 
+        
+    print(f"Updating {skill}")
+
+
+if __name__ == "__main__":
+    app()
+
