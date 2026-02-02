443468747fedb4b733aca509ffe7f670289e27a9
choneface <72355636+choneface@users.noreply.github.com>
2026-02-02 17:44:58 -0500

    add documentation to cli help screens (#4)

--- src/zxdf/main.py
+++ src/zxdf/main.py
@@ -4,18 +4,24 @@ import typer
 
 from zxdf.skill_manager import addSkill
 
-app = typer.Typer()
+app = typer.Typer(help="CLI tool for managing AI skills. Interact with the CLI using skill slugs (author/skillname)")
 console = Console()
 
 @app.command()
-def add(skill: str):
+def add(skill: Annotated[str, typer.Argument(help="[bold]format: author/skillname")]):
+    """
+    Add skill by providing skill slug
+    """
     with console.status("[bold green]Adding skill...") as status:
         addSkill(skill)
     console.print("[bold green]Skill added")
 
 
 @app.command()
-def update(skill: Annotated[str, typer.Argument()] = ""):
+def update(skill: Annotated[str, typer.Argument(help="[bold]format: author/skillname or leave empty to update all")] = ""):
+    """
+    Update a skill by providing its slug or update all by running without skill slug
+    """
     if skill == "":
         ans = input("Are you sure you want to update all skills? (Y/n): ")
         if ans == "Y":
