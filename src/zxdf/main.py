fd71e5d0637912718ea868c0a3527e24b2963efc
choneface <gavin.garcia0001@gmail.com>
2026-02-04 19:36:51 -0500

    create two subcommands (skill, tool)

--- src/zxdf/main.py
+++ src/zxdf/main.py
@@ -6,9 +6,15 @@ from zxdf.skill_manager import addSkill, getAllSkills, updateSkills
 from zxdf.skill_picker import skillPicker, confirm
 
 app = typer.Typer(help="CLI tool for managing AI skills. Interact with the CLI using skill slugs (author/skillname)")
+skills_app = typer.Typer()
+tools_app = typer.Typer()
+
+app.add_typer(skills_app, name="skill")
+app.add_typer(tools_app, name="tool")
+
 console = Console()
 
-@app.command()
+@skills_app.command()
 def add(skill: Annotated[str, typer.Argument(help="[bold]format: author/skillname")]):
     """
     Add skill by providing skill slug
@@ -18,7 +24,7 @@ def add(skill: Annotated[str, typer.Argument(help="[bold]format: author/skillnam
     console.print("[bold green]Skill added")
 
 
-@app.command()
+@skills_app.command()
 def update(
         skill: Annotated[str, typer.Argument(help="[bold]format: author/skillname or leave empty to launch skill picker")] = "",
         update_all: Annotated[bool, typer.Option(help="[bold]indicates all skills should be updated")] = False,
